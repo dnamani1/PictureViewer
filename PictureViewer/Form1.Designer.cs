ee75e2b69a496b2958e1f678c03a137b98ad6a8b
dnamani1 <dnamani1@my.westga.edu>
2024-01-09 19:01:53 -0500

    This is the initial commit to check the set up of the application.

--- PictureViewer/Form1.Designer.cs
+++ PictureViewer/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(976, 515);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Picture Viewer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
